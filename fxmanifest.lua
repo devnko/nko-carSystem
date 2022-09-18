f94d5e029e1f80268136c45d982d6a78485e7a12
devnko <79944577+devnko@users.noreply.github.com>
2022-09-18 23:29:56 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,3 +1,28 @@
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+creater 'nkò#7777'
 fx_version 'adamant'
 games { 'gta5' }
 
@@ -10,3 +35,83 @@ server_scripts {
 	 'config.lua',
 	 'client.lua'
  }
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
+
+------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------ nkò#7777 ------------
+------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------ nkò's shop ------------
+------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------ discord.gg/9WkvWKbJeF ------------
